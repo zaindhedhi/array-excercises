05af9a0859e20c2be813fa29e08691577f24420c
zaindhedhi <zaindhedhi@oulook.com>
2024-08-18 12:54:46 +0500

    1-assigment

---
+++ app.js
@@ -0,0 +1,16 @@
+let fruits = ['apple' , 'grapes' , 'annar'];
+let number = [20 , 30 , 50 , 60]
+let boolean = [true , false]
+
+// console.log(boolean[1])
+
+let mixedArray = ['apple' , 'bannana' , 20 , 30 , true , false]
+// console.log(mixedArray[2])
+
+
+let qualifications = ['HSC' , 'SSC' , 'BCS' , 'B.COM' , 'BS' , 'CS' , 'MASTERS' , 'M.PHILL']
+console.log(qualifications)
+
+let para = document.querySelector('#para')
+
+para.innerHTML = qualifications
